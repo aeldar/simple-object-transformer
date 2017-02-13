6aaea3fd4bd80ee4a7e39ecac718ccc44ea02029
Eldar Aliyev <eldar.aliyev8@gmail.com>
2017-02-13 10:45:52 +0500

    Add tests for wrong rule types

--- src/index.spec.js
+++ src/index.spec.js
@@ -28,6 +28,33 @@ describe('transformer()', () => {
     const target = transformer(RULES, SOURCE);
     expect(target).to.be.deep.equal(EXPECTED_TARGET);
   });
+  describe('throws error in case of unsupported rule type', () => {
+    it('for rule as array', () => {
+      const rules = Object.freeze({ id: [] });
+      const fn = transformer.bind(rules, SOURCE);
+      expect(fn).to.throw(Error);
+    });
+    it('for rule as object', () => {
+      const rules = Object.freeze({ id: {} });
+      const fn = transformer.bind(rules, SOURCE);
+      expect(fn).to.throw(Error);
+    });
+    it('for rule as number', () => {
+      const rules = Object.freeze({ id: 3 });
+      const fn = transformer.bind(rules, SOURCE);
+      expect(fn).to.throw(Error);
+    });
+    it('for rule as null', () => {
+      const rules = Object.freeze({ id: null });
+      const fn = transformer.bind(rules, SOURCE);
+      expect(fn).to.throw(Error);
+    });
+    it('for rule as undefined', () => {
+      const rules = Object.freeze({ id: undefined });
+      const fn = transformer.bind(rules, SOURCE);
+      expect(fn).to.throw(Error);
+    });
+  });
 });
 
 describe('transformerFactory()', () => {
