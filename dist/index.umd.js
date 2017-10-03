a5d01ff2b246b180b8e495bfdf11ad7450ebed3f
Eldar Aliyev <eldar.aliyev8@gmail.com>
2017-10-03 23:57:21 +0300

    Add rimraf

---
+++ dist/index.umd.js
@@ -0,0 +1,37 @@
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.SimpleObjectTransformer = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+var transformer = exports.transformer = function transformer(rules, source) {
+  return Object.keys(rules).reduce(function (target, name) {
+    var rule = rules[name];
+    switch (typeof rule === 'undefined' ? 'undefined' : _typeof(rule)) {
+      case 'string':
+        return _extends({}, target, _defineProperty({}, name, source[rule]));
+      case 'function':
+        return _extends({}, target, _defineProperty({}, name, rule(source, name)));
+      default:
+        throw new Error('Wrong transformer rule for property: ' + name);
+    }
+  }, {});
+};
+
+var transformerFactory = exports.transformerFactory = function transformerFactory(rules) {
+  return function (source) {
+    return transformer(rules, source);
+  };
+};
+
+exports.default = transformerFactory;
+
+},{}]},{},[1])(1)
+});
